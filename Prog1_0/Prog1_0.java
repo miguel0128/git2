5a186e61353c45a5a8289556c6ba946cba90c0da
Miguel F.A. <mfigalo996@educaand.es>
2023-06-12 04:56:10 +0200

    Prog1_0

---
+++ Prog1_0/Prog1_0.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Prog1_0 {
+
+	public static void main(String[] args) {
+	
+		Scanner leerradio = new Scanner(System.in);
+		int radio;
+		System.out.println("Introduce el radio de la esfera");
+		radio=leerradio.nextInt();
+
+	}
+
+}
